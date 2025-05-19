3de85bbbc095fbae3136900599f3ddd23172fea6
Suhaila Hassan <140956614+Suhaila-Hassan@users.noreply.github.com>
2025-05-19 21:43:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -73,4 +73,4 @@ def predict(data: CustomerData):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
